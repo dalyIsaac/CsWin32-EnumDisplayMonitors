59701e307f251b1f423a65af0af667f57777ed55
Isaac Daly <isaac.daly@outlook.com>
2022-12-04 13:22:55 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+
+class MonitorEnumCallback
+{
+    public int Count = 0;
+
+    public virtual unsafe BOOL Callback(HMONITOR monitor, HDC hdc, RECT* rect, LPARAM param)
+    {
+        Count++;
+        return (BOOL)true;
+    }
+}
+
+internal class Program
+{
+    private static unsafe void Main(string[] args)
+    {
+        MonitorEnumCallback closure = new();
+        MONITORENUMPROC proc = new(closure.Callback);
+        bool result = PInvoke.EnumDisplayMonitors(null, null, proc, IntPtr.Zero);
+
+        if (result)
+        {
+            Console.WriteLine($"Found {closure.Count} monitors");
+        }
+        else
+        {
+            Console.WriteLine("EnumDisplayMonitors failed");
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# CsWin32-EnumDisplayMonitors
+
+Reproduction of [microsoft/CsWin32#818](https://github.com/microsoft/CsWin32/issues/818).
